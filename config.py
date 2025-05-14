455de530ec2a0bee6c6dc0b07f5e4d67229b81f0
Alwaa <74717334+Alwaa@users.noreply.github.com>
2025-05-14 14:57:59 +0200

    cleanup config

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ import torch
 RAMPVO_ENV = os.environ.get("RAMPVO_ENV", "server").lower()
 
 ########### General ##############
-SERVER_CUDA_NUMBER = 7
 # 1200 => Around 10GB; 0 => No bound!
 QUEUE_BUFFER_SIZE = 1200 
 # When to start the evaluation NB! Queue should load faster than is used on avg!
@@ -17,19 +16,16 @@ IMU_TESTING = False
 
 ### Location Specific & Overrides ###
 if RAMPVO_ENV == "server":
-    # os.environ["CUDA_VISIBLE_DEVICES"] = f"{SERVER_CUDA_NUMBER}"
     cuda_visible = os.environ.get("CUDA_VISIBLE_DEVICES")
-    assert not (cuda_visible is None), "!On the server you need to set!:\n CUDA:_VISIBLE_DEVICES\n"
-    assert len(cuda_visible) > 0, "!On the server you need to set!:\n CUDA:_VISIBLE_DEVICES\n"
+    assert cuda_visible is not None and len(cuda_visible), "!On the server you need to set enviroment variable!:\n CUDA_VISIBLE_DEVICES\n"
 
     LOADING_THREAD_TORCH_INTRA_OP_THREAD_NUM = 2
-    TARTAN_PATH_PREFIX    = "/data/storage"
+    TARTAN_PATH_PREFIX = "/data/storage"
 else:
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     LOADING_THREAD_TORCH_INTRA_OP_THREAD_NUM = 1
     TARTAN_PATH_PREFIX    = ""
 #####################################
-
 
 
 ############ SPESIFICS && EDGE CASE HANDLING ##############
@@ -41,15 +37,11 @@ QUEUE_ASYNC_SLEEP_BETWEEN_STARTUP_CHECKS = 1.0
 ###########################################################
 
 
-# OpenMP/MKL/BLAS
-torch.set_num_threads(GENERAL_TORCH_INTRA_OP_THREAD_NUM)
-# Inter-op pool
-torch.set_num_interop_threads(4) #NOT CURRENTLY A FACTOR? MAYBE FOR TRAINING?
+torch.set_num_threads(GENERAL_TORCH_INTRA_OP_THREAD_NUM) # OpenMP/MKL/BLAS
+torch.set_num_interop_threads(4)# Inter-op pool #NOT CURRENTLY A FACTOR? MAYBE FOR TRAINING?
 
-print("\nintra-op threads:", torch.get_num_threads())
-print("inter-op threads:", torch.get_num_interop_threads())
+print(f"\nThreads:\n\tintra-op:{torch.get_num_threads()} | inter-op: {torch.get_num_interop_threads()}")
 print("CurrentGPU", os.environ.get("CUDA_VISIBLE_DEVICES"))
-
 print(f"\n NB! For [Evaluation] you should see {LOADING_THREAD_TORCH_INTRA_OP_THREAD_NUM} + {GENERAL_TORCH_INTRA_OP_THREAD_NUM}\
       number of threads with active CPU usage.")
 
